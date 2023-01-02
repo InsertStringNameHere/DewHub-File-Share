89077ad265d57834dc9d6f3954eda4ff05591b6f
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2023-01-02 17:04:13 +1100

    Initial Commit

---
+++ FileShare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Dynamic;
+
+namespace FileShare
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.zgaf.io/api_v1/maps/Transit%20Wars/file"; ;
+            string file = @"sandbox.map";
+
+            Console.WriteLine("Test Download:");
+            Console.ReadLine();
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(url, file);
+            }
+        }
+    }
+}
