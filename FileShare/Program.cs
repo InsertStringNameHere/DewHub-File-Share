c4844b37fded489c7b609baef5ad23fb399da1ab
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2023-01-05 13:56:45 +1100

    Add Download Options
    Can now download both variants and maps. Still needs to be tied into javascript functions in CEF UI

--- FileShare/Program.cs
+++ FileShare/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 using System.Dynamic;
+using Newtonsoft.Json.Linq;
 
 namespace FileShare
 {
@@ -13,16 +14,74 @@ namespace FileShare
     {
         static void Main(string[] args)
         {
-            string url = "https://api.zgaf.io/api_v1/maps/Transit%20Wars/file"; ;
-            string file = @"sandbox.map";
+            string fileType;
+            string id;
 
-            Console.WriteLine("Test Download:");
-            Console.ReadLine();
+            Console.WriteLine("Input File Type: ");
+            fileType = Console.ReadLine();
 
-            using (WebClient client = new WebClient())
+            switch (fileType) 
             {
-                client.DownloadFile(url, file);
+                case "map":
+                    fileType = "maps";
+                break;
+                case "variant":
+                    fileType = "variants";
+                break;
+                case "prefab":
+                    fileType = "prefabs";
+                break;
+                case "mod":
+                    fileType = "mods";
+                break;
             }
+
+            Console.WriteLine("Input File ID: ");
+            id = Console.ReadLine();
+
+            WebClient client = new WebClient();
+
+            string url = "https://api.zgaf.io/api_v1/"+fileType+"/" + id;
+            string response = client.DownloadString(url);
+
+            var data = JObject.Parse(response);
+
+            string fileUrl = "";
+            string fileDirectory = "";
+            string fileName = "";
+
+            switch (fileType) 
+            {
+                case "maps":
+                    fileUrl = "https://api.zgaf.io/api_v1/maps/"+data["mapName"]+"/file";
+                    fileDirectory = "maps/" + data["mapName"] + "/";
+                    fileName = "sandbox.map";
+                break;
+                case "variants":
+                    fileUrl = "https://api.zgaf.io/api_v1/variants/"+data["id"]+"/file";
+                    fileDirectory = "variants/" + data["variantName"] + "/";
+                    fileName = "" + data["variantFileName"] + "";
+                break;
+                case "prefabs":
+                    fileUrl = "https://api.zgaf.io/api_v1/prefabs/"+data["prefabName"]+"/file";
+                    fileDirectory = "prefabs/" + data["prefabName"] + "/";
+                    fileName = "" + data["prefabFileName"] + "";
+                break;
+                case "mods":
+                    fileUrl = "https://api.zgaf.io/api_v1/mods/"+data["modName"]+"/file";
+                    fileDirectory = "mods/" + data["modName"] + "/";
+                    fileName = "" + data["modFileName"] + "";
+               break;
+            }
+
+            using (client = new WebClient()) 
+            {
+                Directory.CreateDirectory(fileDirectory);
+                client.DownloadFile(fileUrl, fileDirectory + fileName);
+                Console.WriteLine("Download Complete");
+            }
+
+            Console.ReadLine();
         }
     }
 }
