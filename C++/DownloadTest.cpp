2fb6d9f4558112032e32b119beae996f0014ba34
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2023-01-16 01:00:12 +1100

    Add SDL Event Manager
    Very Unstable (Need to implement into ElDorito to see how well it actually works)

--- C++/DownloadTest.cpp
+++ C++/DownloadTest.cpp
@@ -1,3 +1,4 @@
+#define SDL_MAIN_HANDLED
 #define CURL_STATICLIB
 #include <iostream>
 #include <string>
@@ -8,11 +9,9 @@
 #include <memory>
 #include <thread>
 
+#include <SDL.h>
 #include "curl/curl.h"
 
-//Synchronous Downloading
-
-/*
 size_t write_to_file(void* contents, size_t size, size_t nmemb, void* userp)
 {
     size_t realsize = size * nmemb;
@@ -21,138 +20,72 @@ size_t write_to_file(void* contents, size_t size, size_t nmemb, void* userp)
     return realsize;
 }
 
-int main(void)
+int main()
 {
     CURL* curl;
+    CURLM* multi_handle;
+    int still_running;
     CURLcode res;
-    char url[] = "https://api.zgaf.io/api_v1/maps/13/file";
+    std::string url = "https://api.zgaf.io/api_v1/maps/13/file";
+
     curl = curl_easy_init();
-    if (curl)
+    if (curl) 
     {
         static std::ofstream file("sandbox.map", std::ios::binary);
-        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_file);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
-    return 0;
-}
-*/
 
-//Asynchronous Downloading
+        multi_handle = curl_multi_init();
+        curl_multi_add_handle(multi_handle, curl);
 
-using EasyHandle = std::unique_ptr<CURL, std::function<void(CURL*)>>;
-EasyHandle CreateEasyHandle()
-{
-    auto curl = EasyHandle(curl_easy_init(), curl_easy_cleanup);
-    if (!curl)
-    {
-        throw std::runtime_error("Failed creating CURL easy object");
-    }
-    return curl;
-}
-
-using MultiHandle = std::unique_ptr<CURLM, std::function<void(CURLM*)>>;
-MultiHandle CreateMultiHandle()
-{
-    auto curl = MultiHandle(curl_multi_init(), curl_multi_cleanup);
-    if (!curl)
-    {
-        throw std::runtime_error("Failed creating CURL multi object");
-    }
-    return curl;
-}
-
-size_t write_to_file(void* contents, size_t size, size_t nmemb, void* userp)
-{
-    size_t realsize = size * nmemb;
-    auto file = reinterpret_cast<std::ofstream*>(userp);
-    file->write(reinterpret_cast<const char*>(contents), realsize);
-    return realsize;
-}
-
-timeval get_timeout(CURLM* multi_handle)
-{
-    long curl_timeo = -1;
-    struct timeval timeout;
-    timeout.tv_sec = 1;
-    timeout.tv_usec = 0;
-
-    curl_multi_timeout(multi_handle, &curl_timeo);
-    if (curl_timeo >= 0) {
-        timeout.tv_sec = curl_timeo / 1000;
-        if (timeout.tv_sec > 1)
-            timeout.tv_sec = 1;
-        else
-            timeout.tv_usec = (curl_timeo % 1000) * 1000;
-    }
-    return timeout;
-}
-
-int wait_if_needed(CURLM* multi_handle, timeval& timeout)
-{
-    fd_set fdread;
-    fd_set fdwrite;
-    fd_set fdexcep;
-    FD_ZERO(&fdread);
-    FD_ZERO(&fdwrite);
-    FD_ZERO(&fdexcep);
-    int maxfd = -1;
-
-    auto mc = curl_multi_fdset(multi_handle, &fdread, &fdwrite, &fdexcep, &maxfd);
-
-    if (mc != CURLM_OK) {
-        std::cerr << "curl_multi_fdset() failed, code " << mc << "." << std::endl;
-    }
-
-    if (maxfd == -1) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-
-    int rc = maxfd != -1 ? select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout) : 0;
-    return rc;
-}
-
-void multi_loop(CURLM* multi_handle)
-{
-    int still_running = 0;
-
-    curl_multi_perform(multi_handle, &still_running);
-
-    while (still_running) {
-        struct timeval timeout = get_timeout(multi_handle);
-
-        auto rc = wait_if_needed(multi_handle, timeout);
-
-        if (rc >= 0)
+        if (SDL_Init(SDL_INIT_TIMER) < 0) 
         {
-            curl_multi_perform(multi_handle, &still_running);
+            std::cerr << "SDL initialization failed" << std::endl;
+            return 1;
         }
+
+        SDL_Event event;
+        bool quit = false;
+
+        while (!quit) 
+        {
+            while (SDL_PollEvent(&event) != 0) 
+            {
+                if (event.type == SDL_QUIT) 
+                {
+                    quit = true;
+                    break;
+                }
+            }
+            SDL_Delay(100);
+
+            while (CURLM_CALL_MULTI_PERFORM == curl_multi_perform(multi_handle, &still_running));
+
+            if (still_running) 
+            {
+                int numfds;
+                CURLMcode mc = curl_multi_wait(multi_handle, NULL, 0, 1000, &numfds);
+                if (mc != CURLM_OK) 
+                {
+                    std::cerr << "curl_multi_wait() failed: " << curl_multi_strerror(mc) << std::endl;
+                    break;
+                }
+            }
+        }
+
+        res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, url.c_str());
+
+        if (res != CURLE_OK) 
+        {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+        curl_multi_cleanup(multi_handle);
+
+        file.close();
     }
-}
-
-int main(void)
-{
-    std::list<EasyHandle> handles(1);
-    MultiHandle multi_handle;
-
-    multi_handle = CreateMultiHandle();
-    std::for_each(handles.begin(), handles.end(), [](auto& handle) {handle = CreateEasyHandle(); });
-
-    std::for_each(handles.begin(), handles.end(), [](auto& handle) 
-    {
-        static std::ofstream file("sandbox.map", std::ios::binary);
-        curl_easy_setopt(handle.get(), CURLOPT_URL, "https://api.zgaf.io/api_v1/maps/13/file");
-        curl_easy_setopt(handle.get(), CURLOPT_WRITEFUNCTION, write_to_file);
-        curl_easy_setopt(handle.get(), CURLOPT_WRITEDATA, &file);
-    });
-
-    std::for_each(handles.begin(), handles.end(), [&multi_handle](auto& handle) {curl_multi_add_handle(multi_handle.get(), handle.get()); });
-    
-    multi_loop(multi_handle.get());
-    
-    std::for_each(handles.begin(), handles.end(), [&multi_handle](auto& handle) {curl_multi_remove_handle(multi_handle.get(), handle.get()); });
 
     return 0;
 }
