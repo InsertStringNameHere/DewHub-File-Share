7b4df3e79da3280fca25b5701b4a3fee6c91a1b6
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2023-01-16 19:16:08 +1100

    Add setup for zip file extraction

--- C++/DownloadTest.cpp
+++ C++/DownloadTest.cpp
@@ -1,15 +1,7 @@
-#define SDL_MAIN_HANDLED
 #define CURL_STATICLIB
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <functional>
-#include <list>
-#include <algorithm>
-#include <memory>
-#include <thread>
 
-#include <SDL.h>
 #include "curl/curl.h"
 
 size_t write_to_file(void* contents, size_t size, size_t nmemb, void* userp)
@@ -22,70 +14,42 @@ size_t write_to_file(void* contents, size_t size, size_t nmemb, void* userp)
 
 int main()
 {
-    CURL* curl;
-    CURLM* multi_handle;
-    int still_running;
-    CURLcode res;
-    std::string url = "https://api.zgaf.io/api_v1/maps/13/file";
+	std::string savepath = "Forerunner Prefab Pack.zip";
+	std::string url = "https://api.zgaf.io/api_v1/prefabs/9/file";
 
-    curl = curl_easy_init();
-    if (curl) 
-    {
-        static std::ofstream file("sandbox.map", std::ios::binary);
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_file);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+	CURL* curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if (curl)
+	{
+		static std::ofstream file(savepath, std::ios::binary);
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_file);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+		res = curl_easy_perform(curl);
 
-        multi_handle = curl_multi_init();
-        curl_multi_add_handle(multi_handle, curl);
+		switch (res)
+		{
+		    case CURLE_OK:
+		    	std::cout << "Done!" << std::endl;
+		    	break;
+		    case CURLE_COULDNT_RESOLVE_HOST:
+		    	std::cout << "Failed to resolve host" << std::endl;
+		    	break;
+		    case CURLE_COULDNT_CONNECT:
+		    	std::cout << "A connection error occured" << std::endl;
+		    	break;
+		    case CURLE_WRITE_ERROR:
+		    	std::cout << "An error occured when trying to save the file" << std::endl;
+		    	break;
+		    default:
+		    	std::cout << "An unknown error occured" << std::endl;
+		    	break;
+		}
 
-        if (SDL_Init(SDL_INIT_TIMER) < 0) 
-        {
-            std::cerr << "SDL initialization failed" << std::endl;
-            return 1;
-        }
-
-        SDL_Event event;
-        bool quit = false;
-
-        while (!quit) 
-        {
-            while (SDL_PollEvent(&event) != 0) 
-            {
-                if (event.type == SDL_QUIT) 
-                {
-                    quit = true;
-                    break;
-                }
-            }
-            SDL_Delay(100);
-
-            while (CURLM_CALL_MULTI_PERFORM == curl_multi_perform(multi_handle, &still_running));
-
-            if (still_running) 
-            {
-                int numfds;
-                CURLMcode mc = curl_multi_wait(multi_handle, NULL, 0, 1000, &numfds);
-                if (mc != CURLM_OK) 
-                {
-                    std::cerr << "curl_multi_wait() failed: " << curl_multi_strerror(mc) << std::endl;
-                    break;
-                }
-            }
-        }
-
-        res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, url.c_str());
-
-        if (res != CURLE_OK) 
-        {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        curl_easy_cleanup(curl);
-        curl_multi_cleanup(multi_handle);
-
-        file.close();
-    }
+		curl_easy_cleanup(curl);
+		file.close();
+	}
 
     return 0;
 }
